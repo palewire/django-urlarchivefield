9f45632a9778e56a68c6bd899499ae819927d4bd
palewire <ben.welsh@gmail.com>
2014-06-27 22:16:09 -0700

    Some basics

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # django-urlarchivefield
+
+This is a joint project of [PastPages.org](http://pastpages.org), The Reynolds Journalism Institute and the University of Missouri.
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='django-urlarchivefield',
+    version='0.0.1',
+    description='',
+    author='Ben Welsh',
+    author_email='ben.welsh@gmail.com',
+    url='https://github.com/pastpages/django-urlarchivefield',
+    license='MIT',
+    packages=('urlarchivefield',),
+    install_requires=(
+        'storytracker==0.0.1',
+    ),
+)
