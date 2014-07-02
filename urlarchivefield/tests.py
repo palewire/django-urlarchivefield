3a5d46dc5b7bce16c877e41d14a096903c7c31d8
palewire <ben.welsh@gmail.com>
2014-07-02 07:45:36 -0700

    Better test coverage

--- urlarchivefield/tests.py
+++ urlarchivefield/tests.py
@@ -1,8 +1,10 @@
 #! -*- coding: utf-8 -*-
 import os
+import six
 import shutil
 import tempfile as tempfile
 from django.db import models
+from datetime import datetime
 from django.test import TestCase
 from .fields import URLArchiveField
 from django.test.utils import override_settings
@@ -30,11 +32,15 @@ class URLArchiveTests(TestCase):
         self.url = "http://www.latimes.com"
         self.url2 = "http://www.cnn.com"
 
+    def assertIsInstance(self, value, type):
+        self.assertTrue(isinstance(value, type))
+
     def test_archive(self):
         obj = TestModel.objects.create(archive=self.url)
+        self.assertFalse(obj.archive.name == self.url)
         self.assertTrue(os.path.exists(obj.archive.path))
-        obj.archive.archive_url
-        obj.archive.archive_timestamp
+        self.assertIsInstance(obj.archive.archive_url, six.string_types)
+        self.assertIsInstance(obj.archive.archive_timestamp, datetime)
         obj.archive = self.url2
         obj.save()
         self.assertTrue(os.path.exists(obj.archive.path))
@@ -42,6 +48,12 @@ class URLArchiveTests(TestCase):
         obj.archive.delete()
         with self.assertRaises(ValueError):
             obj.archive.path
-
+        obj = TestModel()
+        obj.save()
+        obj.archive.name
+        with self.assertRaises(ValueError):
+            obj.archive.path
+        obj.archive = None
+        obj.save()
 
 
